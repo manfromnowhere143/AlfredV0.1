684bd2600c104316ec8ae4f8bae5074b97918199
Daniel Wahnish <cogitoergosum143@gmail.com>
2025-12-29 06:01:12 -0800

    Add database to transpilePackages

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  transpilePackages: ['@alfred/core', '@alfred/llm'],
+  transpilePackages: ['@alfred/core', '@alfred/llm', '@alfred/database'],
 };
-
 module.exports = nextConfig;
